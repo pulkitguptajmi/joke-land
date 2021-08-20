a74d14c1cad718e8566b4007d2e92502592ee362
Pulkit Pradeep Gupta <pulkit.jmi@gmail.com>
2021-08-20 17:52:42 +0530

    Create parseJsonJokesService.js

---
+++ parseJsonJokesService.js
@@ -0,0 +1,34 @@
+let allJokesArray = []
+
+function convertParsedJsonJokeToString(parsedJsonJoke) {
+    allJokesArray.push(parsedJsonJoke.joke);
+    return `${parsedJsonJoke.id}|${parsedJsonJoke.joke}|\r\n`;
+}
+
+
+module.exports = (jsonJokes) => {
+    let linesOfJokeExtractedFromJsonResponse = ''
+    let numberOfPages = 0
+    let parsedJsonJokes;
+    try {
+
+        try {
+          parsedJsonJokes = JSON.parse(jsonJokes);
+        } catch (error) {
+          console.log("The JSON response received from the server isn't properly formatted. Pl try again.")
+        }
+
+        if ('results' in parsedJsonJokes) {
+            parsedJsonJokes.results.forEach((parsedJsonJoke) => {
+                linesOfJokeExtractedFromJsonResponse += convertParsedJsonJokeToString(parsedJsonJoke);
+            });
+            numberOfPages += (parsedJsonJokes.total_jokes / parsedJsonJokes.limit).toFixed();
+        } else {
+            linesOfJokeExtractedFromJsonResponse += convertParsedJsonJokeToString(parsedJsonJokes);
+        }
+    }catch (error) {
+        console.log({ error: error, jsonJokes: jsonJokes });
+    }
+
+    return { linesOfJokeExtractedFromJsonResponse, numberOfPages, allJokesArray};
+};
